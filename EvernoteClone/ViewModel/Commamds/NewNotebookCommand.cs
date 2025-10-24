d316697ee8ca93f5246f727a45cdf2eedf3d8c61
Eunsung Cho <eunsung.cho0901@gmail.com>
2025-10-24 20:00:48 +1300

    Initial commit

---
+++ EvernoteClone/ViewModel/Commamds/NewNotebookCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EvernoteClone.ViewModel.Commamds
+{
+    internal class NewNotebookCommand : ICommand
+    {
+        public NotesVM NotesVM { get; set; }
+        public event EventHandler? CanExecuteChanged;
+
+        public NewNotebookCommand(NotesVM vm)
+        {
+            NotesVM = vm;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            // TODO: create new notebook
+            NotesVM.CreateNotebook();
+        }
+    }
+}
